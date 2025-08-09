221b737d3e4561596bc8b20e78c2f6a5f0d408b3
HaojieZhang123 <haojiezhang123@gmail.com>
2025-08-09 22:47:03 +0200

    [FEAT] Step 3: created path to /movies/{id} and /songs/{id} with respective methods and templates

--- src/main/java/org/lessons/java/best_of_the_year/controllers/BOTYController.java
+++ src/main/java/org/lessons/java/best_of_the_year/controllers/BOTYController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("/")
@@ -19,24 +19,71 @@ public class BOTYController {
     }
 
     private Movie[] getBestMovies() {
-        Movie[] movies = new Movie[3];
+        Movie[] movies = new Movie[5];
         movies[0] = new Movie(1, "Interstellar");
         movies[1] = new Movie(2, "The Imitation Game");
         movies[2] = new Movie(3, "Hidden Figures");
+        movies[3] = new Movie(4, "Good Will Hunting");
+        movies[4] = new Movie(5, "The Theory of Everything");
         return movies;
     }
 
     private Song[] getBestSongs() {
-        Song[] songs = new Song[3];
-        songs[0] = new Song(1, "The Days");
-        songs[1] = new Song(2, "Shape of You");
-        songs[2] = new Song(3, "Viva La Vida");
+        Song[] songs = new Song[5];
+        songs[0] = new Song(1, "Bohemian Rhapsody");
+        songs[1] = new Song(2, "Thriller");
+        songs[2] = new Song(3, "Let It Be");
+        songs[3] = new Song(4, "Imagine");
+        songs[4] = new Song(5, "Like a Rolling Stone");
         return songs;
+    }
+
+    @GetMapping("/movies/{id}")
+    public String movieDetail(@PathVariable("id") int id, Model model) {
+
+        Movie[] movies = getBestMovies();
+
+        Movie movie = null;
+        for (Movie element : movies) {
+            if (element.getId() == id) {
+                movie = element;
+            }
+        }
+
+        if (movie != null) {
+            model.addAttribute("title", movie.getTitle());
+            return "movieDetail";
+        } else {
+            model.addAttribute("title", "Movie not found");
+            return "movieDetail";
+        }
+
+    }
+
+    @GetMapping("/songs/{id}")
+    public String songDetail(@PathVariable("id") int id, Model model) {
+
+        Song[] songs = getBestSongs();
+
+        Song song = null;
+        for (Song element : songs) {
+            if (element.getId() == id) {
+                song = element;
+            }
+        }
+
+        if (song != null) {
+            model.addAttribute("title", song.getTitle());
+            return "songDetail";
+        } else {
+            model.addAttribute("title", "Song not found");
+            return "songDetail";
+        }
+
     }
 
     @GetMapping("/movies")
     public String bestMovies(Model model) {
-
         String bestMovies = "";
         Movie[] movies = getBestMovies();
         for (Movie movie : movies) {
@@ -45,12 +92,10 @@ public class BOTYController {
 
         model.addAttribute("bestMovies", bestMovies);
         return "movies";
-
     }
 
     @GetMapping("/songs")
     public String bestSongs(Model model) {
-
         String bestSongs = "";
         Song[] songs = getBestSongs();
         for (Song song : songs) {
@@ -59,7 +104,6 @@ public class BOTYController {
 
         model.addAttribute("bestSongs", bestSongs);
         return "songs";
-
     }
 
 }
